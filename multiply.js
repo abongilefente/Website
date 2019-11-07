19c3468e1ef63b7019235adf0c7e42aa78df76a9
Fente Abongile <AFente@oldmutual.com>
2019-11-07 09:36:50 +0200

    first commit 3

--- multiply.js
+++ multiply.js
@@ -1,13 +1,47 @@
 function addNumbers()
-                {
-                        var r = parseInt(document.getElementById("InterestRate").value);
-                        var p = parseInt(document.getElementById("Borrowed ").value);
-						var n = parseInt(document.getElementById("installments").value);
-                        var ansD = document.getElementById("answer");
-                        //ansD.value = ((((r/100)/12)*p)*(Math.pow((1+((r/100)/12)),n*12)))/ Math.pow((1+((r/100)/12)),(n*12))-(n*12);
-						ansD.value = (r/100/12*p)/
-									(    1-(Math.pow((1+(r/100/12)),n*12))     );	
+	 {
+	var r = parseInt(document.getElementById("InterestRate").value);
+	var p = parseInt(document.getElementById("Borrowed ").value);
+	var n = parseInt(document.getElementById("installments").value);
+	var ansD = document.getElementById("answer");
+        //ansD.value = ((((r/100)/12)*p)*(Math.pow((1+((r/100)/12)),n*12)))/ Math.pow((1+((r/100)/12)),(n*12))-(n*12);
+	ansD.value = ((r/100/12)*p*(Math.pow((1+(r/100/12)),n*12)))/(    (Math.pow((1+(r/100/12)),n*12)) - 1    );	
+	
+	}
+				
+				
 
-										
-						
-                }
+
+window.onload = function () {
+    var chart = new CanvasJS.Chart("chartContainer",
+    {
+
+      title:{
+      text: "Bond vs Time "
+      },
+      axisX: {
+        valueFormatString: "1",
+        interval:10,
+        intervalType: "year"
+      },
+      axisY:{
+        includeZero: true
+      },
+      data: [
+      {
+        type: "line",
+
+        dataPoints: [
+        { x: 0, y: 30 },
+        { x: 20, y: 25},
+          { x: 30, y: 20},
+        { x: 65, y: 10 },
+        { x: 100, y: 5 },
+        
+        ]
+      }
+      ]
+    });
+
+    chart.render();
+  }
